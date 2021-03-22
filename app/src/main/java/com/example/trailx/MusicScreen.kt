9879ebd605c6c1b64abc57589b173ac5b39320b4
aish21 <aish.akshu@gmail.com>
2021-03-22 22:37:51 +0800

    Spotify API

--- app/src/main/java/com/example/trailx/MusicScreen.kt
+++ app/src/main/java/com/example/trailx/MusicScreen.kt
@@ -1,11 +1,20 @@
 package com.example.trailx
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+
 
 class MusicScreen : AppCompatActivity() {
+    private val CLIENT_ID = "6945fbec77614c7fb910a4baf1358056"
+    private val REDIRECT_URI = "http://671065406800-4rtd4b0cdqntnpak5mc0ftov1q3fjla9.apps.googleusercontent.com"
+    private var mSpotifyAppRemote: SpotifyAppRemote? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_screen)
@@ -23,7 +32,10 @@ class MusicScreen : AppCompatActivity() {
         }
         val discover_new_trails_bt_bar = findViewById<Button>(R.id.discover_new_trails_bt_music)
         discover_new_trails_bt_bar.setOnClickListener{
-            val intent_discover_new_trails_bt_bar = Intent(this, DiscoverNewTrailsScreen::class.java)
+            val intent_discover_new_trails_bt_bar = Intent(
+                this,
+                DiscoverNewTrailsScreen::class.java
+            )
             startActivity(intent_discover_new_trails_bt_bar)
         }
         val active_trail_bt_bar = findViewById<Button>(R.id.active_trail_bt_music)
@@ -46,5 +58,37 @@ class MusicScreen : AppCompatActivity() {
             val intent_in_app_music_bt = Intent(this, InAppMusicScreen::class.java)
             startActivity(intent_in_app_music_bt)
         }
+    }
+
+    protected override fun onStart() {
+        super.onStart()
+        val connectionParams = ConnectionParams.Builder(CLIENT_ID)
+            .setRedirectUri(REDIRECT_URI)
+            .showAuthView(true)
+            .build()
+        SpotifyAppRemote.connect(this, connectionParams,
+            object : Connector.ConnectionListener {
+                override fun onConnected(spotifyAppRemote: SpotifyAppRemote) {
+                    mSpotifyAppRemote = spotifyAppRemote
+                    Log.d("MusicScreen", "Connected! Yay!")
+
+                    // Now you can start interacting with App Remote
+                    connected()
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Log.e("MusicScreen", throwable.message, throwable)
+
+                    // Something went wrong when attempting to connect! Handle errors here
+                }
+            })
+    }
+    private fun connected() {
+        // Then we will write some more code here.
+        mSpotifyAppRemote?.getPlayerApi()?.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+    }
+    protected override fun onStop() {
+        super.onStop()
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 }
