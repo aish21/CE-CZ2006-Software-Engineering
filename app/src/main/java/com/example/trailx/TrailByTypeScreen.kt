b095f6a4749fdc258878e055b9e1c1618e548187
Aishwarya Singh <aishwarya@Aishwaryas-MacBook-Air.local>
2021-03-31 16:56:33 +0800

    Trails by type

--- app/src/main/java/com/example/trailx/TrailByTypeScreen.kt
+++ app/src/main/java/com/example/trailx/TrailByTypeScreen.kt
@@ -1,9 +1,12 @@
 package com.example.trailx
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.Style
 import kotlinx.android.synthetic.main.activity_active_trail_screen.*
@@ -11,24 +14,44 @@ import kotlinx.android.synthetic.main.activity_active_trail_screen.mapView_activ
 import kotlinx.android.synthetic.main.activity_trail_by_type_screen.*
 
 class TrailByTypeScreen : AppCompatActivity() {
+    private lateinit var type_textDisplay: TextView
+    private lateinit var name_textDisplay: TextView
+    var check_trail:Boolean = false
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trail_by_type_screen)
+        supportActionBar?.hide()
 
-        // Mapbox access token is configured here. This needs to be called either in your application
-        // object or in the same activity which contains the mapview.
-        Mapbox.getInstance(this, getString(R.string.access_token))
+        type_textDisplay = findViewById<TextView>(R.id.trail_type_trail_by_type)
+        name_textDisplay = findViewById<TextView>(R.id.name_trail_by_type)
 
-        // This contains the MapView in XML and needs to be called after the access token is configured.
-        setContentView(R.layout.activity_trail_by_type_screen)
-        mapView_trails_by_type?.onCreate(savedInstanceState)
-        mapView_trails_by_type?.getMapAsync { mapboxMap ->
-            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-                Style.Builder().fromUri("mapbox://styles/ashsongh/ckmt73aky3hpc17l93d4rwbbt")
-                // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+        val nature_bt = findViewById<Button>(R.id.nature_trail_bt_type_trail)
+        nature_bt.setOnClickListener{
+            name_textDisplay.text = "Bedok Reservoir Loop Trail"
+            type_textDisplay.text = "Nature Trail"
+            Toast.makeText(this, "Trail found!", Toast.LENGTH_SHORT).show()
+            global.choice = 2
+            check_trail = true
+        }
+
+        val city_bt = findViewById<Button>(R.id.city_trail_bt_type_trail)
+        city_bt.setOnClickListener{
+            name_textDisplay.text = "Telok Ayer Park Trail"
+            type_textDisplay.text = "City Trail"
+            Toast.makeText(this, "Trail found!", Toast.LENGTH_SHORT).show()
+            global.choice = 1
+            check_trail = true
+        }
+
+        val start_trail_bt_type = findViewById<Button>(R.id.start_trail_trails_by_type)
+        start_trail_bt_type.setOnClickListener{
+            if(check_trail){
+                startActivity(Intent(this@TrailByTypeScreen, ActiveTrailScreen::class.java))
             }
         }
-        supportActionBar?.hide()
+
         val back_to_home_bt_bar = findViewById<Button>(R.id.back_to_home_bt_trails_by_type)
         back_to_home_bt_bar.setOnClickListener{
             val intent_back_to_home_bt_bar = Intent(this, HomeScreen::class.java)
@@ -59,41 +82,5 @@ class TrailByTypeScreen : AppCompatActivity() {
             val intent_music_bt_bar = Intent(this, MusicScreen::class.java)
             startActivity(intent_music_bt_bar)
         }
-    }
-
-    // Add the mapView lifecycle to the activity's lifecycle methods
-    public override fun onResume() {
-        super.onResume()
-        mapView_active_trail?.onResume()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView_active_trail?.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView_active_trail?.onStop()
-    }
-
-    public override fun onPause() {
-        super.onPause()
-        mapView_active_trail?.onPause()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView_active_trail?.onLowMemory()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView_active_trail?.onDestroy()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView_active_trail?.onSaveInstanceState(outState)
     }
 }
